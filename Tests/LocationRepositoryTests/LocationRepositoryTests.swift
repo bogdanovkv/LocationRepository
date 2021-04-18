7b31f1a241440ce316e6f3d8db91540f59f3a6cf
Konstatin Bogdanov <>
2021-04-18 19:37:18 +0300

    Repos

---
+++ Tests/LocationRepositoryTests/LocationRepositoryTests.swift
@@ -0,0 +1,10 @@
+import XCTest
+@testable import LocationRepository
+
+final class LocationRepositoryTests: XCTestCase {
+    func testExample() {}
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
